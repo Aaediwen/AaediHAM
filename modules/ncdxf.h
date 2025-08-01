edec3c6c70c2bdbe1c3ea0a082a41b9e803b9187
Aaediwen <aaediwen@gmail.com>
2025-08-01 09:00:08 -0400

    const extern patch
    Fixed a const extern line that MSVC didn't like

--- modules/ncdxf.h
+++ modules/ncdxf.h
@@ -1,10 +1,18 @@
+#ifndef NCDXF_H
+
 #include "../aaediclock.h"
 
+
+#define NCDXF_H
 struct beacon {
     char call[10];
     std::string location;
-} const extern beacons[18];
+};
 
-const extern double beacon_freqs[5];
+extern const struct beacon beacons[18];
+
+extern const double beacon_freqs[5];
 
 void ncdxf_module(ScreenFrame& panel);
+
+#endif
