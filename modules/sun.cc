860b069ba12ec4abec954d7454b9e20a473728dd
Aaediwen <aaediwen@gmail.com>
2025-11-13 15:40:58 -0500

    Add files via upload

--- modules/sun.cc
+++ modules/sun.cc
@@ -6,9 +6,8 @@
 #include <SDL3_image/SDL_image.h>
 
 SDL_TimerID sdo_timer = 0;
-SDL_Mutex *sdo_mutex =0;
 SDL_Texture* SDO_Texture = nullptr;
-SDL_Surface* SDO_Surface = nullptr;
+
 
 bool refresh_icon_flag = false;
 void fetch_sdo () {
@@ -20,14 +19,7 @@ void fetch_sdo () {
          if (data_size) {
              debug_log << "SOLAR: Loaded image size: " << data_size << " bytes\n";
              add_data_cache(MOD_SOLAR, data_size, (void*)raw_image);
-             SDL_LockMutex(sdo_mutex);
-             SDL_IOStream *imgdata = SDL_IOFromConstMem((void*)raw_image, data_size);
-             SDO_Surface = IMG_Load_IO(imgdata, true);
-             if (SDO_Surface) {
-                 refresh_icon_flag = true;
-             }
-             SDL_UnlockMutex(sdo_mutex);
-
+             refresh_icon_flag = true;
          } else {
              debug_log << "Failed\n";
              debug_log << "SDO Fetch Failed\n";
@@ -48,10 +40,6 @@ void sdo_image(ScreenFrame& panel, time_t timestamp) {
     debug_log.flush();
     Uint32 data_size = 0;
     time_t cache_time;
-    if (!sdo_mutex) {
-        sdo_mutex=SDL_CreateMutex();
-    }
-//    int reload_flag =0;
     char* raw_image = 0 ;
     if (!sdo_timer) {
         sdo_timer=SDL_AddTimer(60, fetch_sdo, NULL);
@@ -104,34 +92,36 @@ void sdo_image(ScreenFrame& panel, time_t timestamp) {
         solar_pin.tooltip[0]=0;
         delete_owner_pins(MOD_SOLAR);
         debug_log << "SOLAR: Read " << data_size << " bytes of data\n";
-        SDL_LockMutex(sdo_mutex);
+        SDL_Surface* SDO_Surface = nullptr;
         try {
-            // recreate the SDO GPU texture as needed
-           if (refresh_icon_flag && SDO_Surface) {
-               if (SDO_Texture) {
-                   SDL_DestroyTexture(SDO_Texture);
-                   SDO_Texture=0;
-               }
-               SDO_Texture = SDL_CreateTextureFromSurface(panel.GetRenderer(), SDO_Surface);
-           }
-           // render the sun to the panel
-           if (SDO_Texture) {
-               SDL_SetRenderTarget(panel.GetRenderer(), panel.texture);
-               SDL_RenderTexture(panel.GetRenderer(), SDO_Texture, NULL, NULL);
-           } else {
-               panel.render_text(SDL_FRect{2,2,panel.dims.w,(panel.dims.h/10)}, Sans, SDL_Color{255,0,0,0}, "NO SDO DATA");
-           }
-           // update the icon as needed
-           if (refresh_icon_flag || !icon_bin.get_icon(map_icons::ICON_SUN) ) {
-                if (!SDO_Surface) {
-                    debug_log << "SOLAR: Refreshing sun icon\n";
-                    data_size = cache_loader(MOD_SOLAR, (void**)&raw_image, &cache_time);
-                    if (data_size > 10) {
-                        SDL_IOStream *imgdata = SDL_IOFromConstMem((void*)raw_image, data_size);
-                        SDO_Surface = IMG_Load_IO(imgdata, true);
-                    }
+            // load the image from cache
+            if (refresh_icon_flag || !icon_bin.get_icon(map_icons::ICON_SUN) ) {
+                data_size = cache_loader(MOD_SOLAR, (void**)&raw_image, &cache_time);
+                if (data_size > 10) {
+                    SDL_IOStream *imgdata = SDL_IOFromConstMem((void*)raw_image, data_size);
+                    SDO_Surface = IMG_Load_IO(imgdata, true);
                 }
-
+            }
+            // recreate the SDO GPU texture as needed
+            if (refresh_icon_flag) {
+                if (SDO_Texture) {
+                    SDL_DestroyTexture(SDO_Texture);
+                    SDO_Texture=0;
+                }
+                if (SDO_Surface) {
+                    SDL_Log ("Loaded SDO Texture");
+                    SDO_Texture = SDL_CreateTextureFromSurface(panel.GetRenderer(), SDO_Surface);
+                }
+            }
+            // render the sun to the panel
+            if (SDO_Texture) {
+                SDL_SetRenderTarget(panel.GetRenderer(), panel.texture);
+                SDL_RenderTexture(panel.GetRenderer(), SDO_Texture, NULL, NULL);
+            } else {
+                panel.render_text(SDL_FRect{2,2,panel.dims.w,(panel.dims.h/10)}, Sans, SDL_Color{255,0,0,0}, "NO SDO DATA");
+            }
+            // update the icon as needed
+            if (refresh_icon_flag || !icon_bin.get_icon(map_icons::ICON_SUN) ) {
                 if (SDO_Surface) {
                     SDL_Surface* icon_surface = SDL_CreateSurface(100, 100, SDL_PIXELFORMAT_RGBA32);
                     if (SDL_BlitSurfaceScaled(SDO_Surface, NULL, icon_surface, NULL, SDL_SCALEMODE_NEAREST)) {
@@ -152,7 +142,6 @@ void sdo_image(ScreenFrame& panel, time_t timestamp) {
             SDL_DestroySurface(SDO_Surface);
             SDO_Surface = 0;
         }
-        SDL_UnlockMutex(sdo_mutex);
         if (raw_image) {
             free (raw_image);
             raw_image=0;
