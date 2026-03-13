6cf446e41359450ea9ee2458d4377079bc1f97af
Anthony Creech <aaediwen@gmail.com>
2026-03-13 07:16:08 -0400

    removed module files

--- modules/clock.h
+++
@@ -1,3 +0,0 @@
-#include "../aaediclock.h"
-
-int draw_clock(ScreenFrame& panel, TTF_Font* font);
--- modules/map.h
+++
@@ -1,5 +0,0 @@
-#include "aaediclock.h"
-
-//void load_maps(SDL_Renderer* surface, SDL_FRect size);
-int draw_map(ScreenFrame& panel);
-void draw_overlays(ScreenFrame& panel);
